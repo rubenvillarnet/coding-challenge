fff9a1df62f48b148ded28c1c9398c028337fee0
Rubén Villar Grela <ruben@rubenvillar.net>
2019-06-13 02:28:30 +0200

    modified CORS log

--- back/app.js
+++ back/app.js
@@ -18,7 +18,7 @@ mongoose
 
 const app = express()
 
-var allowedOrigins = ['http://localhost:3000', 'http://localhost:5000', 'https://fullstack-coding-challenge.herokuapp.com/']
+var allowedOrigins = ['http://localhost:3000', 'http://localhost:5000', 'https://fullstack-coding-challenge.herokuapp.com']
 app.use(
   cors({
     origin: function(origin, callback) {
@@ -26,7 +26,7 @@ app.use(
       // (like mobile apps or curl requests)
       if (!origin) return callback(null, true)
       if (allowedOrigins.indexOf(origin) === -1) {
-        var msg = 'The CORS policy for this site does not ' + 'allow access from the specified Origin.'
+        var msg = 'The CORS policy for this site does not ' + 'allow access from the specified Origin: ' + origin
         return callback(new Error(msg), false)
       }
       return callback(null, true)
